e73c55ade4b606b1f0a915955a8704c597f440ad
healingfields <dihratene@gmail.com>
2022-03-03 23:39:08 +0100

    added the content models and migrated

--- courses/models.py
+++ courses/models.py
@@ -1,5 +1,9 @@
+from distutils.command.upload import upload
+from lib2to3.pytree import Base
 from django.db import models
 from django.contrib.auth.models import User
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
 
 class Subject(models.Model):
     title = models.CharField(max_length=150)
@@ -32,4 +36,35 @@ class Module(models.Model):
 
     def __str__(self) -> str:
         return self.title
+
+class Content(models.Model):
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE, limit_choices_to={'model__in':('text',
+    'video',
+    'image',
+    )})
+    object_id = models.PositiveIntegerField()
+    item = GenericForeignKey('content_type', 'object_id')
+
+class ItemBase(models.Model):
+    title = models.CharField(max_length=150)
+    created = models.DateTimeField(auto_now_add=True)
+    owner = models.ForeignKey(User, related_name='%(class)s_related', on_delete=models.CASCADE)
+
+    class Meta:
+        abstract = True
+
+    def __str__(self):
+        return self.title
+    
+class Text(ItemBase):
+    content = models.TextField()
+
+class File(ItemBase):
+    file = models.FileField(upload_to='files')
+
+class Image(ItemBase):
+    image = models.FileField(upload_to='images')
+
+class Video(ItemBase):
+    url = models.URLField()
 
