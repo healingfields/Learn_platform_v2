89777cae836fc3a72f5737b8d3a97247b6054a25
healingfields <dihratene@gmail.com>
2022-03-04 17:04:01 +0100

    added the authentification functionnality

--- config/urls.py
+++ config/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-
+from django.contrib.auth.views import LoginView, LogoutView
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/login', LoginView.as_view(template_name='auth/login.html'), name='login'),
+    path('account/logout', LogoutView.as_view(), name='logout')
+
+
 ]
